dc81d03355be4b39b98755d4076cf4ec8d35a8ea
Cole Wagner <colewagner632@gmail.com>
2025-03-11 16:02:37 -0500

    Create unit tests for validate_excel()

---
+++ tests/test_validation.py
@@ -0,0 +1,60 @@
+"""Unit tests for validation.py."""
+
+import pandas as pd
+import pytest
+
+from utils.validation import validate_excel
+
+
+@pytest.mark.parametrize(
+    ("sheet_df", "sheet_schema", "expected_exception", "expected_message"),
+    [
+        (
+            pd.DataFrame(),
+            {"column1": "int"},
+            ValueError,
+            "Sheet is empty.",
+        ),
+        (
+            pd.DataFrame({"column1": [1, 2, 3]}),
+            {"column1": "int", "column2": "float"},
+            ValueError,
+            r"Missing columns in sheet. Expected:",
+        ),
+        (
+            pd.DataFrame({"column1": [1, 2, "three"]}),
+            {"column1": "int"},
+            ValueError,
+            r"Column 'column1' cannot be converted to int:",
+        ),
+    ],
+)
+def test_validate_excel_exceptions(
+    sheet_df: pd.DataFrame,
+    sheet_schema: dict,
+    expected_exception: ValueError,
+    expected_message: str,
+) -> None:
+    """
+    Test possible error-causing edge cases for validate_excel() to ensure
+    proper errors are raised.
+    Cases:
+    - Empty sheet
+    - Missing columns
+    - Column with data that cannot be cast to expected type.
+    """
+    with pytest.raises(expected_exception, match=expected_message):
+        validate_excel(sheet_df, sheet_schema)
+
+
+def test_validate_excel_success() -> None:
+    """Test valid case for validate_excel() to ensure proper output."""
+    test_df = pd.DataFrame({
+        "column1": [1, 2, 3],
+        "column2": [1.1, 2.2, 3.3],
+    })
+    sheet_schema = {"column1": "int", "column2": "float"}
+    expected_dtypes = {"column1": "int", "column2": "float"}
+    validated_df = validate_excel(test_df, sheet_schema)
+    for col, dtype in expected_dtypes.items():
+        assert validated_df[col].dtype == dtype
