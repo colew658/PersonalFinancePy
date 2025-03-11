c8fa9aa978f749dde21b85c3e4ddfc9b1ab75bfe
Cole Wagner <colewagner632@gmail.com>
2025-03-11 13:49:48 -0500

    Create project management tools

---
+++ setup.sh
@@ -0,0 +1,42 @@
+python_version="3.12"
+
+usage() {
+    echo: "Usage: $0 [OPTIONS]"
+    echo: "Options:"
+    echo: "  -h, --help: Display this help message"
+    echo: "  -v, --python-version: Python version to use"
+}
+
+log() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
+}
+
+
+log "Cleaning up previous build"
+if [[ "$VIRTUAL_ENV" ]]; then
+    log "Deactivating virtual environment"
+    deactivate > /dev/null
+fi
+
+jupyter kernelspec uninstall venv -y
+rm -rf venv
+log "Previous build cleaned up"
+
+log "Setting up new build"
+log "Creating virtual environment"
+python3 -m pip install virtualenv > /dev/null
+
+log "Configuring virtual environment"
+python3 -m virtualenv venv --python=python${python_version} > /dev/null
+source venv/bin/activate
+log "Virtual environment configured"
+
+log "installing dependencies"
+pip install -e .
+
+log "Installing pre-commit hooks"
+pre-commit install
+pre-commit clean
+
+log "Adding virtual environment to Jupyter"
+python -m ipykernel install --user --name=venv
---
+++ tests/test_expense_tracker.py
@@ -0,0 +1,6 @@
+"""Unit tests for expense_tracker.py."""
+
+
+def test_pass() -> None:
+    """Ensure that the test suite is working."""
+    assert True
